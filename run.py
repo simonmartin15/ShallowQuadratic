7abdd24c8b73902bc986bae3db730c556461d70d
simon-martin-5140 <161721992+simon-martin-5140@users.noreply.github.com>
2024-02-29 16:57:14 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,9 @@
 import subprocess
 
-
 def main():
     I = [1, 2, 4, 5, 6]
     for i in I:
-        print('Figure{}'.format(i))
         subprocess.run(["python", "Figure{}.py".format(i)])
-        print('')
-
 
 if __name__ == '__main__':
     main()
